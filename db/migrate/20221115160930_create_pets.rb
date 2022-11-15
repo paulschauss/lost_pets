28605de85e7b8a0e10cd8547fd2ff3788ff6a62e
paul schauss <p+github@schauss.fr>
2022-11-15 17:10:41 +0100

    rails new

---
+++ db/migrate/20221115160930_create_pets.rb
@@ -0,0 +1,12 @@
+class CreatePets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :species
+      t.integer :age
+      t.date :date_capture
+
+      t.timestamps
+    end
+  end
+end
