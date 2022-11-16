1b82897d8246e1850404a910a1af7dc444427ce0
paul schauss <p+github@schauss.fr>
2022-11-16 11:34:04 +0100

    wip

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,7 @@
 class Pet < ApplicationRecord
+  SPECIES = ['turtle', 'cat', 'dog', 'licorn']
+
+  validates :name, presence: true
+  validates :species, inclusion: { in: SPECIES,
+    message: "%{value} is not a valid specie" }
 end
